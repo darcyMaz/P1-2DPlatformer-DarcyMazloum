72cfcdfefe616a6ce64ff6de68ffeb3aae89b6dd
Darcy Mazloum <damazinator11@gmail.com>
2026-02-27 14:22:57 -0500

    Jump Event Pseudo Code
    I have copied the PlayerMovement scriped and changed the jump mechanic to use callback events instead of polling. At the moment it only uses pseudo code so does not work.

--- Assets/Player/Scripts/PlayerMovement_JumpEvents.cs
+++ Assets/Player/Scripts/PlayerMovement_JumpEvents.cs
@@ -2,32 +2,11 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/*
- * These explainers can be found in the Readme as well.
- * 
- * Character Movement Explanation:
- *      I decided to use RigidBody2D for my character because
- *      1) I get to easily manipulate the velocity of the RigidBody (calculate it using SmoothDamp and just multiply that speed by the direction), 
- *      2) I know that it will collide properly with the static colliders since the RigidBody is tracked by the physics engine,
- *      3) I can set the RigidBody to have interpolate=interpolate and collision detection=continuous to avoid common bugs, and 
- *      4) RigidBodys, where the loop is FixedUpdate(), are perfect for 2D platformers where we rely on forces from the physics engine and precise collisions.
- * 
- * Polling vs. Callback Methods:
- *      I decided to use polling for both the movement and the jump because I needed frame by frame precision for both.
- *      I know that you recommend using callbacks for jumps, but it was critical for me to know the exact frame when the jump button was pressed and released.
- *      For example, when the Jump has its velocity cut when the player releases the jump button on the way up. There is one frame where the jump button 
- *          was released AND they are still jumping AND they are still moving upwards. Having all these states be known was critical. By polling I can
- *          easily access the status of the button while also tracking other logic.
- *          
- * Ground Detection Method: RayCast
- *      I decided to use the RayCast for my ground detection for its precision. It allowed the code to have zero frames of ground detection after the 
- *      jump had been performed.
- *      
- * Coyote Time or Buffered Jump: Both
- *      I decided to include both.
- */
+// This script's jump functionalities don't work.
+// The jump is written out in psuedo code.
+// I will likely get back to this.
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement_JumpEvents : MonoBehaviour
 {
     // Input Actions
     private ProjectActions actionSystem;
@@ -68,6 +47,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float TerminalSpeed = 15f;
     [SerializeField] float FallMultiplier = 2.5f;
 
+
     private void Awake()
     {
         actionSystem = new ProjectActions();
@@ -78,6 +58,9 @@ public class PlayerMovement : MonoBehaviour
         move = actionSystem.Player.Move;
         jump = actionSystem.Player.Jump;
         dash = actionSystem.Player.Dash;
+
+        jump.performed += PressJump;
+        jump.canceled += ReleaseJump;
 
         move.Enable();
         jump.Enable();
@@ -112,15 +95,19 @@ public class PlayerMovement : MonoBehaviour
 
         Move();
         // If Jump is in the Update function, then the IsGrounded check does not work properly.
-        Jump();
+        // Jump();
         // Making the fall feel heavier goes in the FixedUpdate function as it messes with gravity which is part of the games physics.
         HeavyFall();
+        
         
     }
 
     private void Update()
     {
-
+        // JumpChecks()
+        //      A function that would, every frame,
+        //      (1) check if we've left the ground without jumping and start the coyote time counter and
+        //      (2) when we land on the ground, check if the jump buffer timer > 0, if so perform a jump.
     }
 
     private void Move()
@@ -162,6 +149,23 @@ public class PlayerMovement : MonoBehaviour
         return (input > 0) ? (int) Math.Ceiling(input): (input < 0) ? (int) Math.Floor(input): 0;
     }
     
+    // Now this function will be called whenever space bar is pressed.
+    private void PressJump(InputAction.CallbackContext action)
+    {
+        // if is grounded or coyote time > 0
+        //      jump
+
+        // else
+        //      start the jump buffer timer
+
+    }
+    // This function will be called whenever space bar is released.
+    private void ReleaseJump(InputAction.CallbackContext action)
+    {
+        // if rb.linearvelocityY > 0 and !IsGrounded
+        //      cut the linear velocity by the FeelFeel value (in half)
+    }
+
     private void Jump()
     {
         float jumping = jump.ReadValue<float>();
